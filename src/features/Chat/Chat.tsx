158f5392bdf2c4c6786090b276f88fe7c7f345cd
allen.trinh <allen.trinh@rtslabs.com>
2024-03-29 15:33:31 -0400

    feat: import css, add secondary fetch to extract ids

--- src/features/Chat/Chat.tsx
+++ src/features/Chat/Chat.tsx
@@ -4,9 +4,11 @@ import ChatFab from "./components/ChatFab"
 import ChatHeader from "./components/ChatHeader"
 import ChatInput from "./components/ChatInput"
 import ChatWindow from "./components/ChatWindow"
-import useFetchLazy from "./hooks/useFetchLazy"
 import useHighlighter from "./hooks/useHighlighter"
+import useMessenger from "./hooks/useMessenger"
 import { type ChatMessage } from "./types/ChatMessage"
+
+import "../../../src/chat.css"
 
 const initialMessageState = {
   id: new Date().toISOString(),
@@ -24,7 +26,7 @@ function Chat() {
   const [isChatOpen, setIsChatOpen] = useState<boolean>(true)
   const inputRef = useRef<HTMLInputElement>(null)
 
-  const [getData, { response, error, isLoading }] = useFetchLazy(
+  const [getData, { response, error, isLoading }] = useMessenger(
     `completions?prompt=${message?.message}`
   )
   const highlightElement = useHighlighter()
@@ -61,12 +63,34 @@ function Chat() {
       {
         ...initialMessageState,
         id: new Date().toISOString(),
-        message: response.data.ip,
+        message:
+          response?.data?.choices[0]?.message?.content ||
+          "Unable to process response",
         date: new Date().toDateString(),
         timestamp: new Date().toISOString()
       }
     ])
-    // highlightElement("Example_computer_program")
+
+    const extractPrompt = `
+      Examine this response. Extract an id from the response. I only want the id, not the entire response. Do not
+      include any special characters. I should be able to take your response and use it as a variable in my code. For
+      example, if you send back "root", I should be able to take your response and run it through a
+      document.getElementById('root') in JavaScript. If you find text within quotes, send back the quoted text without
+      the quotes. Your response should not contain any special characters or spaces.
+
+      Here is the response:
+
+      ${response?.data?.choices[0]?.message?.content}
+    `
+    fetch(`http://127.0.0.1:8000/api/completions?prompt=${extractPrompt}`)
+      .then((resp) => {
+        return resp.json()
+      })
+      .then((data) => {
+        console.log({ data })
+
+        highlightElement(data.choices[0].message.content)
+      })
   }, [response])
 
   /**
